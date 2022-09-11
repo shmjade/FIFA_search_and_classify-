f709e23bc7bb82517c20ee2caf07d454f08a1792
shmjade <shm.jade@gmail.com>
2022-09-11 19:29:35 -0300

    adding the menu

--- functions.py
+++ functions.py
@@ -48,6 +48,10 @@ def statistic_entries(hash_table, NUM_ENTRIES):
 	print("Shortest entries: " + str(shortest))
 	
 
+
+
+
+
 # --------------------------------------------------
 # -------------     players.csv    -----------------
 
@@ -62,27 +66,40 @@ def insert_hash_players(hash_players, a_player):
 # - inserts the players on the hash table of players
 # - inserts the name of each player on the Trie Tree
 def read_players_csv(hash_players, root):
+	max_name = 0
+	max_position = 0
+
 	with open("players.csv", "r") as archive:
 		line_count = 0
 		csv_table = csv.reader(archive, delimiter=",")
 		i=0
 		for row in csv_table:
 			if(i!=0):
-				# Insert the player on the hash table
+				if(len(row[1])>max_name):
+					max_name = len(row[1])
+				if(len(row[2])>max_position):
+					max_position = len(row[2])				# Insert the player on the hash table
 				hash_players = insert_hash_players(hash_players, (Player(int(row[0]), row[1], row[2], int(row[3]), int(row[4]), int(row[5]))))
 				# Insert the name of the player on the Trie Tree
 				root.insertTrie(row[1], row[0])     # row[1] is the name and row[0] is the sofifa_id
 			i+=1
+	print("========= MAX NAME = ", max_name)
+	print("========= MAX POSITION = ", max_position)
 	return hash_players, root
 
 def printPlayer_1(hash_players, sofifa_id):
 	i = sofifa_id%NUM_ENTRIES_PLAYERS
 	j = find_player_index(hash_players, sofifa_id)
 	player = hash_players[i][j]
-	print(str(player.getSofifaID()), end="")
-	print(" | " + str(player.getName()), end="")
-	print(" | " + str(player.getPosition()), end="")
-	print(" | " + str(player.getAverage()))
+	print(' {:10s} | {:49s} | {:14s} |  {:1.2f}   | {:6d}'.format(str(player.getSofifaID()), str(player.getName()), player.getPosition(), player.getAverage(), player.getCount()))
+
+
+def printPlayer_2(hash_players, sofifa_id, user_rating):
+	i = sofifa_id%NUM_ENTRIES_PLAYERS
+	j = find_player_index(hash_players, sofifa_id)
+	player = hash_players[i][j]
+	print(str(player.getSofifaID()), end=" | ")
+	print('{:49s} | {:1.2f} | {:6d} | {:1.1f}'.format(str(player.getName()), player.getAverage(), player.getCount()), user_rating)
 
 # --------------------------------------------------
 # -------------     ratings.csv    -----------------
---
+++ menu.py
@@ -0,0 +1,44 @@
+# --------------------------------------------------
+# -------------        menu        -----------------
+
+def menu(root, hash_players):
+	print("\t--- MENU ---")
+	print("Options:")
+	print("player <prefix of the names>")
+	print("user <user ID>")
+	print("top<number of players> <position>")
+	print("tags <list of tags>")
+	print("exit")
+	opt = 0
+	while(opt==0):
+		option = input("Type your search: ")
+		first_word = option.split()[0]
+		opt = 1
+		match first_word.lower():
+			case 'player':
+				search1(option.split()[1], root, hash_players)
+				opt = 1
+			case 'user':
+				opt = 2
+			case 'top':
+				opt = 3
+			case 'tags':
+				opt = 4
+			case 'exit':
+				opt = 5
+			case default:
+				print("INPUT NOT VALID")
+				opt = 0
+	
+		
+
+
+
+# --------------------------------------------------
+# -------------     first search    -----------------
+
+def search1(prefix, root, hash_players):
+	print("----- PLAYERS THAT START WITH " + prefix + " -----")
+	print(' {:10s} | {:49s} | {:14s} | {:7s} | {:7s}'.format("sofifa_id","name of the player", "positions", "rating", 'count'))
+	found = root.searchPrefix(prefix)
+	found.printChildren(found,prefix, hash_players)
